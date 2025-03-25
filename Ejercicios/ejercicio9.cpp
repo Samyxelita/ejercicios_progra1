b89956346c84bdade18803af0af98717feb75273
Samuel <samyxelita@gmail.com>
2025-03-25 11:42:29 +0100

    ejercicio9

---
+++ Ejercicios/ejercicio9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int cantidad_widgets, cantidad_gizmos;
+    const int peso_widget = 75; // Peso en gramos
+    const int peso_gizmo = 112; // Peso en gramos
+
+    // Leer la cantidad de widgets y gizmos
+    cout << "Ingrese la cantidad de widgets: ";
+    cin >> cantidad_widgets;
+    cout << "Ingrese la cantidad de gizmos: ";
+    cin >> cantidad_gizmos;
+
+    // Calcular el peso total del pedido
+    int peso_total = (cantidad_widgets * peso_widget) + (cantidad_gizmos * peso_gizmo);
+
+    // Mostrar el peso total
+    cout << "El peso total del pedido es " << peso_total << " gramos." << endl;
+
+    return 0;
+}
